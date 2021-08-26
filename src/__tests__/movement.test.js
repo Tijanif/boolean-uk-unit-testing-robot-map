b842b104f1c4a5627d1830817373dde780b9b876
Tijani Fulani <Fulani07@gmail.com>
2021-08-26 16:23:15 +0100

    right and up test

--- src/__tests__/movement.test.js
+++ src/__tests__/movement.test.js
@@ -1,17 +1,50 @@
-const { moveLeft } = require("../movement");
+const { moveLeft, moveRight, moveUp, moveDown } = require('../movement');
 
-describe("Test the movement functions", () => {
-  describe("The move left function", () => {
-    it("should move the robot to the left", () => {
+describe('Test the movement functions', () => {
+  describe('The move left function', () => {
+    it('should move the robot to the left', () => {
       const currentPosition = { x: 5, y: 5 };
 
       expect(moveLeft(currentPosition)).toEqual({ x: 4, y: 5 });
     });
 
-    it("should not cross the boundaries", () => {
+    it('should not cross the boundaries', () => {
       const currentPosition = { x: 0, y: 5 };
 
       expect(moveLeft(currentPosition)).toEqual({ x: 0, y: 5 });
+    });
+  });
+  describe('The move right function', () => {
+    it('should move the robot to the right', () => {
+      const currentPosition = { x: 5, y: 5 };
+      expect(moveRight(currentPosition)).toEqual({ x: 6, y: 5 });
+    });
+
+    it('should not cross the boundries', () => {
+      const currentPosition = { x: 10, y: 5 };
+      expect(moveRight(currentPosition)).toEqual({ x: 10, y: 5 });
+    });
+  });
+
+  describe('The move up function', () => {
+    it('should move the robot up', () => {
+      const currentposition = { x: 5, y: 5 };
+      expect(moveUp(currentposition)).toEqual({ x: 5, y: 4 });
+    });
+    it('should not cross the boundries', () => {
+      const currentPosition = { x: 5, y: 0 };
+      expect(moveUp(currentPosition)).toEqual({ x: 5, y: 0 });
+    });
+  });
+
+  describe('The move down function', () => {
+    it('should move the robot down', () => {
+      const currentposition = { x: 5, y: 5 };
+      expect(moveDown(currentposition)).toEqual({ x: 5, y: 4 });
+    });
+    it('should not cross the boundries', () => {
+      const currentPosition = { x: 5, y: 0 };
+      expect(moveDown(currentPosition)).toEqual({ x: 5, y: 0 });
     });
   });
 });
--- src/movement.js
+++ src/movement.js
@@ -3,12 +3,33 @@ const moveLeft = (currentPosition) => {
 
   const newPosition = {
     ...currentPosition,
-    x: currentPosition.x - 1
+    x: currentPosition.x - 1,
   };
 
   return newPosition;
 };
 
+const moveRight = (currentPosition) => {
+  if (currentPosition.x === 10) return currentPosition;
+  const newPosition = {
+    ...currentPosition,
+    x: currentPosition.x + 1,
+  };
+
+  return newPosition;
+};
+
+const moveUp = (currentPosition) => {
+  if (currentPosition.y === 0) return currentPosition;
+  const newPosition = {
+    ...currentPosition,
+    y: currentPosition.y - 1,
+  };
+
+  return newPosition;
+};
 module.exports = {
-  moveLeft
+  moveLeft,
+  moveRight,
+  moveUp,
 };
