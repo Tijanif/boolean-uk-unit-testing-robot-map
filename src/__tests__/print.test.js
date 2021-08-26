2081921825c061be38728b0bbd48a7a19de1d527
Tijani Fulani <Fulani07@gmail.com>
2021-08-26 17:04:11 +0100

    position test

--- src/__tests__/print.test.js
+++ src/__tests__/print.test.js
@@ -1,1 +1,13 @@
-const { printPosition } = require("../print");
+const { printPosition } = require('../print');
+
+describe('Test the print functions', () => {
+  describe('Test the printPosition', () => {
+    it('It should print the robot position', () => {
+      const robotPosition = { x: 5, y: 5 };
+
+      expect(printPosition(robotPosition)).toEqual(
+        `Robot is at { x: ${robotPosition.x}, y: ${robotPosition.y} }`
+      );
+    });
+  });
+});
